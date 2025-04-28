3a86e6c020e2f77714355a75d1bc111c17c5866a
mjsb05 <maurijsb25@gmail.com>
2025-04-28 00:02:33 -0600

    fisrt commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:chat_gpt/constants/const.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          scaffoldBackgroundColor: scaffolbackColor,
+          appBarTheme: AppBarTheme(
+            color: cardColor,
+          )),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
