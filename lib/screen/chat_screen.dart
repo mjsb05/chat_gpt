aa2e545bd8c4a56bf011644eb5ea98b845b73ba1
mjsb05 <maurijsb25@gmail.com>
2025-04-28 20:26:12 -0600

    chat_screen update

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,3 +1,4 @@
+import 'package:chat_gpt/constants/const.dart';
 import 'package:chat_gpt/services/assets_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -11,6 +12,25 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final bool _isTyping = false;
+
+  // Es un campo donde se manejarán campos de texto
+  // es late porque se inicializa luego en el initState
+  late TextEditingController textEditingController;
+
+  //Método que se ejecuta al iniciar el widget
+  @override
+  void initState() {
+    textEditingController = TextEditingController();
+    super.initState();
+  }
+
+// Método que se ejecuta al eliminar el widget
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +45,11 @@ class _ChatScreenState extends State<ChatScreen> {
                 color: Colors.white,
                 fontSize: 30,
                 fontWeight: FontWeight.bold)),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.more_vert_rounded, color: Colors.white))
+        ],
       ),
       body: SafeArea(
         child: Column(children: [
@@ -37,6 +62,33 @@ class _ChatScreenState extends State<ChatScreen> {
           ),
           if (_isTyping) ...[
             const SpinKitThreeBounce(color: Colors.white, size: 18),
+            SizedBox(
+              height: 15,
+            ),
+            Material(
+              color: cardColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      style: const TextStyle(color: Colors.white),
+                      controller: textEditingController,
+                      onSubmitted: (value) {
+                        // TODO send message
+                      },
+                      decoration: const InputDecoration.collapsed(
+                          hintText: "How can I help you?",
+                          hintStyle: TextStyle(color: Colors.grey)),
+                    )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.send, color: Colors.white))
+                  ],
+                ),
+              ),
+            )
           ]
         ]),
       ),
